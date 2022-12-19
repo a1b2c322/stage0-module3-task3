cb3c9bedbafa16a89b0950fd97b743cf5f900604
arastepa <stepanyan2298@gmail.com>
2022-12-19 22:55:09 +0400

    ..

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,13 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
+        int first, second, third;
+
+        first = 1;
+        second = 10;
+        third = 100;
+
+        System.out.println(first + "\n" + second + "\n" + third);
         //write code here
     }
 }
